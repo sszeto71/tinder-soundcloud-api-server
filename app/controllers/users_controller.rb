4b13e80c1d81e0e93ee087341fcaee7ad145ba35
Stan Szeto <stanley.szeto@sbcglobal.net>
2014-11-21 17:31:15 -0800

    Added cor settings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
+
+    def index
+        set_cors_headers
+    end
+
+    def set_cors_headers
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+        headers['Access-Control-Request-Method'] = '*'
+        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    end
+
   def create
+    set_cors_headers
     content_type :json
 
     begin
